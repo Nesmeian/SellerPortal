cd7e8f5c60580a8f4e259af3f58ca0fa82694fc1
Nesmeian <lenya.kozlovskiy.97@mail.ru>
2025-03-04 18:06:34 +0300

    feat:add docker to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,13 @@
+import { useEffect } from 'react'
+
 function App() {
+    useEffect(() => {
+        const fetchData = async (): Promise<void> => {
+            const data = await fetch('http://localhost:3000/orders')
+            console.log(await data.json())
+        }
+        fetchData()
+    }, [])
     return <>HELLO WORLD</>
 }
 
